7f57026ff885b5532196d0a70e19d67f912e5a55
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-09-27 15:10:41 -0300

    Add files via upload

--- sec_pages/main.js
+++ sec_pages/main.js
@@ -1,12 +1,17 @@
 let normal_v_button = document.querySelector('.normal_vers')
 let normal_v_window = document.querySelector('.sec2');
+const freeDownload = document.querySelector('.free_download')
+const freeDownloadText = document.querySelector('.free_download h2')
 
-function goToNormalDownload() {
+function goToInfo() {
     normal_v_window.scrollIntoView({block: "start", behavior: "smooth"})
-    normal_v_button.innerHTML = "Baixando..."
-    setTimeout(function() {
-        normal_v_button.innerHTML = "Download Gratuito"
-    },1200)
 }
 
-normal_v_button.addEventListener('click', goToNormalDownload)
+normal_v_button.addEventListener('click', goToInfo)
+
+freeDownload.addEventListener('click', function() {
+    freeDownloadText.innerHTML = "Baixando..."
+    setTimeout(function() {
+        freeDownloadText.innerHTML = "Download Gratuito"
+    },1200)
+})
