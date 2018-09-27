6169be03d499f6d3a8440c5426dc36c8e37c8476
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-09-27 19:14:39 -0300

    Add files via upload

--- sec_pages/main.js
+++ sec_pages/main.js
@@ -1,7 +1,17 @@
 let normal_v_button = document.querySelector('.normal_vers')
 let normal_v_window = document.querySelector('.sec2');
+
 const freeDownload = document.querySelector('.free_download')
 const freeDownloadText = document.querySelector('.free_download h2')
+const preTitle = document.querySelector('#pre_title') 
+const detailsButton = document.querySelector('#details') 
+const backToMainPage = document.querySelector('#back_main_page') 
+
+// COMPONENTIZAÇÃO DE TEXTOS
+freeDownloadText.innerHTML = "Download Gratuito";
+preTitle.innerHTML = "VOCÊ ESTÁ PRESTES A BAIXAR A PLANILHA DE";
+detailsButton.innerHTML = "Detalhes do Programa";
+backToMainPage.innerHTML = "Página inicial";
 
 function goToInfo() {
     normal_v_window.scrollIntoView({block: "start", behavior: "smooth"})
