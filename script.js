82a867f7663bdb08a34bce1cfb3039fe2cb87d20
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-10-28 15:01:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -95,3 +95,16 @@ function mudarPalitos() {
     palitoMeio.classList.toggle('palitoMeioClicked')
     palitoBaixo.classList.toggle('palitoBaixoClicked')
 }
+
+
+// PWA //
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('./service-worker.js')
+    .then(function() { 
+        console.log('Service Worker Registered'); 
+    }, function(error){
+        console.error(error);
+    });
+}
