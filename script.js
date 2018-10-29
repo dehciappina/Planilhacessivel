a6bd80142d3c4ec73c098afbcd1d4ee091c28bc7
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-10-29 20:13:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -115,9 +115,9 @@ function mudarPalitos() {
       navigator.serviceWorker
         .register('./service-worker.js', {scope: './'})
         .then(function(registration) {
-            console.log('Service Worker Registered', registration)
+            console.log('Service Worker Registered')
         })
         .catch(function(err) {
-            console.log('Service Worker Failed to Register', err)
+            console.log('Service Worker Failed to Register')
         })
   }
